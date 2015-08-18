2943569c08f6c251c99386a9d38ed93757b22755
bebatut <berenice.batut@gmail.com>
2015-08-18 15:04:00 +0200

    Remove useless commented lines and add copy of tool_conf file into galaxy

--- prepare_galaxy_tools.sh
+++ prepare_galaxy_tools.sh
@@ -4,6 +4,8 @@
 #eval "$(boot2docker shellinit)"
 
 #eval "$(boot2docker shellinit)"
+
 docker build -t asaim/extract-similarity-search-report tools/extract/extract_similarity_search_report
 cp tools/extract/extract_similarity_search_report/extract_similarity_search_report.xml ../galaxy/tools/docker/extract_similarity_search_report.xml
 
+cp tool_conf.xml ../galaxy/config/tool_conf.xml
