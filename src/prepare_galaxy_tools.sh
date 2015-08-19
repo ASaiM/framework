43ce6d8709c548bc7971d28dfa545997136575e2
bebatut <berenice.batut@gmail.com>
2015-08-19 14:02:28 +0200

    Add a copy of extract_similarity_search_report without docker

--- src/prepare_galaxy_tools.sh
+++ src/prepare_galaxy_tools.sh
@@ -9,8 +9,12 @@ mkdir -p $galaxy_tool_dir
 
 # Extraction of similarity search report
 extract_similarity_search_report_dir=$tool_dir/extract/extract_similarity_search_report
-docker build -t asaim/extract-similarity-search-report $extract_similarity_search_report_dir
+sudo docker build -t asaim/extract-similarity-search-report $extract_similarity_search_report_dir
 cp $extract_similarity_search_report_dir/extract_similarity_search_report.xml $galaxy_tool_dir/extract_similarity_search_report.xml
+
+mkdir $galaxy_tool_dir/extract_similarity_search_report_2
+cp $tool_dir/extract/extract_similarity_search_report_2/extract_similarity_search_report.py $galaxy_tool_dir/extract_similarity_search_report_2/extract_similarity_search_report.py
+cp $tool_dir/extract/extract_similarity_search_report_2/extract_similarity_search_report.xml $galaxy_tool_dir/extract_similarity_search_report_2/extract_similarity_search_report.xml
 
 # CatDocker
 cp $tool_dir/catDocker/catDocker.xml $galaxy_tool_dir/catDocker.xml
