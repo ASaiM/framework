48ed81b1059029d6293f55a36a494665e9cc87b6
bebatut <berenice.batut@gmail.com>
2015-08-19 16:01:42 +0200

    Add differial docker build given os and add somme modification for extract_similarity_search_report.py

--- src/prepare_galaxy_tools.sh
+++ src/prepare_galaxy_tools.sh
@@ -9,11 +9,16 @@ mkdir -p $galaxy_tool_dir
 
 # Extraction of similarity search report
 extract_similarity_search_report_dir=$tool_dir/extract/extract_similarity_search_report
-sudo docker build -t asaim/extract-similarity-search-report $extract_similarity_search_report_dir
 cp $extract_similarity_search_report_dir/extract_similarity_search_report.xml $galaxy_tool_dir/extract_similarity_search_report.xml
+case "$OSTYPE" in
+  darwin*)  docker build -t asaim/extract-similarity-search-report $extract_similarity_search_report_dir ;; 
+  linux*)   sudo docker build -t asaim/extract-similarity-search-report $extract_similarity_search_report_dir ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
 
-mkdir $galaxy_tool_dir/extract_similarity_search_report_2
-cp $tool_dir/extract/extract_similarity_search_report_2/extract_similarity_search_report.py $galaxy_tool_dir/extract_similarity_search_report_2/extract_similarity_search_report.py
+
+mkdir -p $galaxy_tool_dir/extract_similarity_search_report_2
+cp $tool_dir/extract/extract_similarity_search_report/extract_similarity_search_report.py $galaxy_tool_dir/extract_similarity_search_report_2/extract_similarity_search_report.py
 cp $tool_dir/extract/extract_similarity_search_report_2/extract_similarity_search_report.xml $galaxy_tool_dir/extract_similarity_search_report_2/extract_similarity_search_report.xml
 
 # CatDocker
