168a3c18f35dcf5c4251752ad52cf9067c2ac552
bebatut <berenice.batut@gmail.com>
2015-08-20 08:56:32 +0200

    Remove useless tools, comment docker use, format .xml to follow best practices

--- src/prepare_galaxy_tools.sh
+++ src/prepare_galaxy_tools.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
 galaxy_dir=$1
-galaxy_tool_dir=$galaxy_dir/tools/docker
+galaxy_tool_dir=$galaxy_dir/tools/
 tool_dir=tools
 
-
-mkdir -p $galaxy_tool_dir
-
+# Extraction tools
+galaxy_extract_tool_dir=$galaxy_tool_dir/extract/
+extract_tool_dir=$tool_dir/extract
 # Extraction of similarity search report
-extract_similarity_search_report_dir=$tool_dir/extract/extract_similarity_search_report
-cp $extract_similarity_search_report_dir/extract_similarity_search_report.xml $galaxy_tool_dir/extract_similarity_search_report.xml
+galaxy_extract_similarity_search_report_dir=$galaxy_extract_tool_dir/extract_similarity_search_report
+extract_similarity_search_report_dir=$extract_tool_dir/extract_similarity_search_report
+mkdir -p $galaxy_extract_similarity_search_report_dir
+cp $extract_similarity_search_report_dir/extract_similarity_search_report.py $galaxy_extract_similarity_search_report_dir/extract_similarity_search_report.py
+cp $extract_similarity_search_report_dir/extract_similarity_search_report.xml $galaxy_extract_similarity_search_report_dir/extract_similarity_search_report.xml
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    sudo docker build -t asaim/extract-similarity-search-report $extract_similarity_search_report_dir
-elif [[ "$OSTYPE" == "darwin"* ]] ; then
-    docker build -t asaim/extract-similarity-search-report $extract_similarity_search_report_dir
-else 
-    echo "unknown"
-    exit
-fi
+#if [[ "$OSTYPE" == "linux-gnu" ]]; then
+#    sudo docker build -t asaim/extract-similarity-search-report $extract_similarity_search_report_dir
+#elif [[ "$OSTYPE" == "darwin"* ]] ; then
+#    docker build -t asaim/extract-similarity-search-report $extract_similarity_search_report_dir
+#else 
+#    echo "unknown"
+#    exit
+#fi
 
-mkdir -p $galaxy_tool_dir/extract_similarity_search_report_2
-cp $tool_dir/extract/extract_similarity_search_report/extract_similarity_search_report.py $galaxy_tool_dir/extract_similarity_search_report_2/extract_similarity_search_report.py
-cp $tool_dir/extract/extract_similarity_search_report_2/extract_similarity_search_report.xml $galaxy_tool_dir/extract_similarity_search_report_2/extract_similarity_search_report.xml
 
-# CatDocker
-cp $tool_dir/catDocker/catDocker.xml $galaxy_tool_dir/catDocker.xml
