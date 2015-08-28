dc4bd391d8b830a58b3a683f8edcc7675f7294ed
bebatut <berenice.batut@gmail.com>
2015-08-28 16:52:03 +0200

    Add humann

--- src/prepare_galaxy_tools.sh
+++ src/prepare_galaxy_tools.sh
@@ -2,72 +2,47 @@
 
 galaxy_dir=$1
 galaxy_tool_dir=$galaxy_dir/tools/
-tool_dir=tools
-
-# Usearch
-usearch_tool_path=$tool_dir/usearch/usearch8.0.1623_i86osx32
-
-# Quality control
-galaxy_quality_control_tool_dir=$galaxy_tool_dir/quality_control/
-quality_control_tool_dir=$tool_dir/quality_control
+current_dir=`pwd`
+db_dir=/db/
 
 ## prinseq
-galaxy_prinseq_dir=$galaxy_quality_control_tool_dir/prinseq
-prinseq_dir=$quality_control_tool_dir/prinseq
-
-mkdir -p $galaxy_prinseq_dir/src
-mkdir -p $prinseq_dir/src/
+prinseq_dir=$galaxy_tool_dir/quality_control/prinseq
+mkdir -p $prinseq_dir/src
 curl -L -s http://downloads.sourceforge.net/project/prinseq/standalone/prinseq-lite-0.20.4.tar.gz | tar -C $prinseq_dir/src/ --strip-components=1 -xz
-cp $prinseq_dir/prinseq.xml $galaxy_prinseq_dir/prinseq.xml
-cp -r $prinseq_dir/src/ $galaxy_prinseq_dir/src
 
-# Dereplication
-galaxy_dereplication_tool_dir=$galaxy_tool_dir/dereplication/
-dereplication_tool_dir=$tool_dir/dereplication
+## metaphlan 2
+#metaphlan_dir=$galaxy_tool_dir/non_rRNA_taxonomic_assignation/metaphlan
+#hg clone https://bitbucket.org/biobakery/metaphlan2 $metaphlan_dir/src
 
-## derep_prefix
-galaxy_derep_prefix_dir=$galaxy_dereplication_tool_dir/derep_prefix
-derep_prefix_dir=$dereplication_tool_dir/derep_prefix
-mkdir -p $galaxy_derep_prefix_dir/src/
-cp $derep_prefix_dir/derep_prefix.xml $galaxy_derep_prefix_dir/derep_prefix.xml
-cp -p $usearch_tool_path $galaxy_derep_prefix_dir/src/usearch
+## humann 2
+#humann2_dir=$galaxy_dir/metabolic_analysis/humann2
+#cd $humann2_dir 
+#hg clone https://bitbucket.org/biobakery/humann2 
+#cd humann2 
+#python setup.py install 
+#cd $current_dir
 
-# Extraction tools
-galaxy_extract_tool_dir=$galaxy_tool_dir/extract/
-extract_tool_dir=$tool_dir/extract
+## humann 
+humann_dir=$galaxy_tool_dir/metabolic_analysis/humann
+cd $humann_dir
+if [ ! -d "humann/" ]; then
+    hg clone https://bitbucket.org/biobakery/humann
+fi
+cd $current_dir
 
-## Extraction of sequence file
-galaxy_extract_sequence_file_dir=$galaxy_extract_tool_dir/extract_sequence_file
-extract_sequence_file_dir=$extract_tool_dir/extract_sequence_file
+## retrieve cog, extract info and formate for use with humann
+python data/db/cog/src/download_database.py \
+    --raw_data_dir data/db/cog/raw_data/
+python data/db/cog/src/extract_cog_data.py \
+    --raw_data_dir data/db/cog/raw_data/ \
+    --extracted_data_dir data/db/cog/extracted_data/
+python data/db/cog/src/formate_database_for_humann.py \
+    --raw_data_dir data/db/cog/raw_data/ \
+    --extracted_data_dir data/db/cog/extracted_data/ \
+    --humann_formated_data_dir data/db/cog/humann_formated_data/ \
+    --humann_dir $humann_dir/humann/
 
-mkdir -p $galaxy_extract_sequence_file_dir
-cp $extract_sequence_file_dir/extract_sequence_file.py $galaxy_extract_sequence_file_dir/extract_sequence_file.py
-cp $extract_sequence_file_dir/extract_sequence_file.xml $galaxy_extract_sequence_file_dir/extract_sequence_file.xml
 
-cp -r $extract_sequence_file_dir/test-data/data/read_write_fasta_file/ $galaxy_dir/test-data/read_write_fasta_file
-cp -r $extract_sequence_file_dir/test-data/data/simple_extraction_fasta_file/ $galaxy_dir/test-data/simple_extraction_fasta_file
-cp -r $extract_sequence_file_dir/test-data/data/simple_constraint_fasta_file/ $galaxy_dir/test-data/simple_constraint_fasta_file
-cp -r $extract_sequence_file_dir/test-data/data/double_constraint_fasta_file/ $galaxy_dir/test-data/double_constraint_fasta_file
-cp -r $extract_sequence_file_dir/test-data/data/read_write_fastq_file/ $galaxy_dir/test-data/read_write_fastq_file
-cp -r $extract_sequence_file_dir/test-data/data/split_fastq_file/ $galaxy_dir/test-data/split_fastq_file
-cp -r $extract_sequence_file_dir/test-data/data/simple_extraction_fastq_file/ $galaxy_dir/test-data/simple_extraction_fastq_file
-cp -r $extract_sequence_file_dir/test-data/data/simple_constraint_fastq_file/ $galaxy_dir/test-data/simple_constraint_fastq_file
-cp -r $extract_sequence_file_dir/test-data/data/double_constraint_fastq_file/ $galaxy_dir/test-data/double_constraint_fastq_file
 
-## Extraction of similarity search report
-galaxy_extract_similarity_search_report_dir=$galaxy_extract_tool_dir/extract_similarity_search_report
-extract_similarity_search_report_dir=$extract_tool_dir/extract_similarity_search_report
-mkdir -p $galaxy_extract_similarity_search_report_dir
-cp $extract_similarity_search_report_dir/extract_similarity_search_report.py $galaxy_extract_similarity_search_report_dir/extract_similarity_search_report.py
-cp $extract_similarity_search_report_dir/extract_similarity_search_report.xml $galaxy_extract_similarity_search_report_dir/extract_similarity_search_report.xml
-
-#if [[ "$OSTYPE" == "linux-gnu" ]]; then
-#    sudo docker build -t asaim/extract-similarity-search-report $extract_similarity_search_report_dir
-#elif [[ "$OSTYPE" == "darwin"* ]] ; then
-#    docker build -t asaim/extract-similarity-search-report $extract_similarity_search_report_dir
-#else 
-#    echo "unknown"
-#    exit
-#fi
 
 
