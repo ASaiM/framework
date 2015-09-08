df141d39a1573e87d7488f63c03b7b5d1cb58f06
bebatut <berenice.batut@gmail.com>
2015-09-08 16:13:31 +0200

    Add conditional outputs and start debug humann in galaxy

--- tools/metabolic_analysis/humann/humann_wrapper.py
+++ tools/metabolic_analysis/humann/humann_wrapper.py
@@ -20,16 +20,16 @@ def generate_humann_param_file(sconstruct_filepath, args, data_dir):
     sconstruct_file.write("\n")
 
     # Directory name scanned for input files
-    if args.cog_extracted_data == 'true':
+    if args.cog_extracted_data == 'yes':
         sconstruct_file.write("c_strDirInput = \"" + data_dir + '/' + os.path.split(args.input)[0] + "/formatted\"\n")
     else:
         sconstruct_file.write("c_strDirInput = \"" + data_dir + '/' + os.path.split(args.input)[0] + "/\"\n")
 
     # Directory into which all output files are placed
-    tmp_output_dir = os.path.split(args.input)[0] + '/temporary_humann_output'
+    tmp_output_dir = os.path.split(args.input)[0] + '/temporary_humann_output/'
     if not os.path.exists(tmp_output_dir):
-        os.makedir(tmp_output_dir)
-    sconstruct_file.write("c_strDirOutput = \"" + data_dir + '/' + tmp_output_dir + "\"\n")
+        os.makedirs(tmp_output_dir)
+    sconstruct_file.write("c_strDirOutput = \"" + tmp_output_dir + "\"\n")
 
     # Filename from which metadata annotations are read; can be excluded
     #sconstruct_file.write("c_strInputMetadata                  = c_strDirInput + \"/hmp_metadata.dat\"\n")
@@ -66,7 +66,7 @@ def generate_humann_param_file(sconstruct_filepath, args, data_dir):
     sconstruct_file.write("c_apProcessors = [\n")
 
     # Default: txt or gzipped txt blastx data
-    sconstruct_file.write(" CProcessor( \".txt\",\"00\",\"hit\",c_strProgBlast2Hits,[],[],True,True ),\n")
+    sconstruct_file.write(" CProcessor( \".dat\",\"00\",\"hit\",c_strProgBlast2Hits,[],[],True,True ),\n")
     #sconstruct_file.write(" CProcessor( \".txt.gz\",\"00\",\"hit\", c_strProgBlast2Hits,[],[],True,True ),\n")
 
     # Default: mapped bam data
@@ -90,60 +90,74 @@ def generate_humann_param_file(sconstruct_filepath, args, data_dir):
     #    A list of zero or more files provided on the command line to the processing script
     #    A list of zero or more non-file arguments provided on the command line to the script
 
-    # hits -> enzymes
-    # Generate KO abundances from BLAST hits
-    if args.ko_abundance== 'true':
-        sconstruct_file.write(" CProcessor( \"00\",\"01\",\"enz\",c_strProgHits2Enzymes,[c_strFileKOC, c_strFileGeneLs],[str( c_fOrg )] ),\n")
+    last_id=None
+
+    # KEGG
+    if args.ko_abundance== 'yes':
+        # hits -> enzymes
+
+        # Generate KO abundances from BLAST hits
+        sconstruct_file.write(" CProcessor( \"00\",\"01\",\"ko\",c_strProgHits2Enzymes,[c_strFileKOC, c_strFileGeneLs],[str( c_fOrg )] ),\n")
         sconstruct_file.write(" CProcessor( \"01\",\"01b\",\"cat\",c_strProgCat,[] ),\n")
 
-    # Generate MetaCyc enzyme abundances from BLAST hits
-    # Enable only if c_strInputMetaCyc is defined above
-    if args.metacyc_enz_abundance == 'true':
+        # enzymes -> pathways
+        # Generate KEGG pathway assignments from KOs
+        if args.kegg_pathway == 'yes':
+            sconstruct_file.write(" CProcessor( \"01\",\"02a\",\"mpt\",c_strProgEnzymes2PathwaysMP,[c_strFileMP, c_strFileKEGGC] ),\n")
+            last_id = "02a"
+
+        # Generate KEGG module assignments from KOs
+        if args.kegg_module == 'yes':
+            sconstruct_file.write(" CProcessor( \"01\",\"02a\",\"mpm\",c_strProgEnzymes2PathwaysMP,[c_strFileMP, c_strFileModuleC] ),\n")
+            last_id = "02a"
+
+    # MetaCyc
+    if args.metacyc_enzyme == 'yes':
+        # hits -> enzymes
+        # Generate MetaCyc enzyme abundances from BLAST hits
+        # Enable only if c_strInputMetaCyc is defined above
         sconstruct_file.write(" CProcessor( \"00\",\"11\",\"mtc\",c_strProgHits2Metacyc,[c_strFileMCC] ),\n")
 
-    # hits -> metarep
-    # Optional: Generate a METAREP input file from BLAST hits
-    if args.metarep == 'true' :
+        # enzymes -> pathways
+        # Generate MetaCyc pathway assignments from enzymes
+        if args.metacyc_pathway == 'yes':
+            sconstruct_file.write(" CProcessor( \"11\",\"02a\",\"mpt\",c_strProgEnzymes2PathwaysMP,[c_strFileMP, c_strFileMCPC] ),\n")
+            last_id = "02a"
+
+    # MetaRep
+    if args.metarep == 'yes' :
+        # hits -> metarep
+        # Optional: Generate a METAREP input file from BLAST hits
         sconstruct_file.write(" CProcessor( \"00\",\"99\",\"mtr\",c_strProgHits2Metarep,[c_strFileGeneLs] ),\n")
 
-    # enzymes -> pathways
-    # Generate KEGG pathway assignments from KOs
-    if args.kegg_pathway == 'true':
-        sconstruct_file.write(" CProcessor( \"01\",\"02a\",\"mpt\",c_strProgEnzymes2PathwaysMP,[c_strFileMP, c_strFileKEGGC] ),\n")
+    if last_id != None:
+        # taxonomic provenance
+        if args.taxonomic_limitation == 'yes':
+            sconstruct_file.write(" CProcessor( \"" + last_id + "\",\"02b\",\"cop\",c_strProgTaxlim,[c_strFileTaxPC, c_strFileKOC] ),\n")
+            last_id = "02b"
 
-    # Generate KEGG module assignments from KOs
-    if args.kegg_module == 'true':
-        sconstruct_file.write(" CProcessor( \"01\",\"02a\",\"mpm\",c_strProgEnzymes2PathwaysMP,[c_strFileMP, c_strFileModuleC] ),\n")
+        # smoothing
+        # Smoothing is disabled by default with the latest KEGG Module update
+        if args.wb_smoothing == 'yes':
+            sconstruct_file.write(" CProcessor( \"" + last_id + "\",\"03a\",\"wbl\",c_strProgSmoothWB,[c_strFilePathwayC] ),\n")
+        else:
+            sconstruct_file.write(" CProcessor( \"" + last_id + "\",\"03a\",\"nul\",c_strProgCat,[] ),\n")
+        last_id = "03a"
 
-    # Generate MetaCyc pathway assignments from enzymes
-    if args.metacyc_pathway == 'true':
-        sconstruct_file.write(" CProcessor( \"11\",\"02a\",\"mpt\",c_strProgEnzymes2PathwaysMP,[c_strFileMP, c_strFileMCPC] ),\n")
+        # gap filling
+        if args.gap_filling == 'yes':
+            sconstruct_file.write(" CProcessor( \"" + last_id + "\",\"03b\",\"nve\",c_strProgGapfill,[c_strFilePathwayC] ),\n")
+            last_id = "03b"
 
-    # taxonomic provenance
-    if args.taxonomic_limitation == 'true':
-        sconstruct_file.write(" CProcessor( \"02a\",\"02b\",\"cop\",c_strProgTaxlim,[c_strFileTaxPC, c_strFileKOC] ),\n")
+        # COVERAGE
+        if args.coverage == 'yes':
+            sconstruct_file.write(" CProcessor( \"" + last_id + "\",\"03c\",\"nve\",c_strProgPathCov,[c_strFilePathwayC, c_strFileModuleP] ),\n")
+            if args.low_coverage_elimination == 'yes':
+                sconstruct_file.write(" CProcessor( \"03c\",\"04a\",\"xpe\",c_strProgPathCovXP,[c_strProgXipe] ),\n")
 
-    # smoothing
-    # Smoothing is disabled by default with the latest KEGG Module update\n")
-    if args.wb_smoothing == 'true':
-        sconstruct_file.write(" CProcessor( \"02b\",\"03a\",\"wbl\",c_strProgSmoothWB,[c_strFilePathwayC] ),\n")
-    sconstruct_file.write(" CProcessor( \"02b\",\"03a\",\"nul\",c_strProgCat,[] ),\n")
-
-    # gap filling
-    last_id="03a"
-    if args.gap_filling == 'true':
-        sconstruct_file.write(" CProcessor( \"03a\",\"03b\",\"nve\",c_strProgGapfill,[c_strFilePathwayC] ),\n")
-        last_id = "03b"
-
-    # COVERAGE
-    if args.coverage == 'true':
-        sconstruct_file.write(" CProcessor( \"" + last_id + "\",\"03c\",\"nve\",c_strProgPathCov,[c_strFilePathwayC, c_strFileModuleP] ),\n")
-        if args.low_coverage_elimination == 'true':
-            sconstruct_file.write(" CProcessor( \"03c\",\"04a\",\"xpe\",c_strProgPathCovXP,[c_strProgXipe] ),\n")
-
-    # ABUNDANCE
-    if args.abundance_computation == 'true':
-        sconstruct_file.write(" CProcessor( \"" + last_id + "\",\"04b\",\"nve\",c_strProgPathAb,[c_strFilePathwayC, c_strFileModuleP] ),\n")
+        # ABUNDANCE
+        if args.abundance_computation == 'yes':
+            sconstruct_file.write(" CProcessor( \"" + last_id + "\",\"04b\",\"nve\",c_strProgPathAb,[c_strFilePathwayC, c_strFileModuleP] ),\n")
 
     sconstruct_file.write("]\n")
     
@@ -160,10 +174,14 @@ def generate_humann_param_file(sconstruct_filepath, args, data_dir):
     sconstruct_file.write(" [None, c_strProgZero],\n")
     sconstruct_file.write(" [None, c_strProgFilter, [c_strFilePathwayC, c_strFileModuleP]],\n")
     sconstruct_file.write(" [r'0(1")
-    if args.gap_filling == 'true':
-        sconstruct_file.write("|(3b)")
-    if args.abundance_computation == 'true':
-        sconstruct_file.write("|(4b)")
+    if last_id != None:
+        if args.coverage == '' :
+            if args.low_coverage_elimination == 'yes':
+                sconstruct_file.write("|(4a)")
+            else:
+                sconstruct_file.write("|(3c)")
+        if args.abundance_computation == 'yes':
+            sconstruct_file.write("|(4b)'")
     sconstruct_file.write(")', c_strProgNormalize],\n")
     sconstruct_file.write(" [None, c_strProgEco],\n")
     #sconstruct_file.write("    [None, c_strProgMetadata],\n")#, [c_strInputMetadata]],\n")
@@ -180,12 +198,10 @@ def generate_humann_param_file(sconstruct_filepath, args, data_dir):
     #        The processing script\n")
     #        An array of zero or more files provided on the command line to the processing script\n")
     #    A required tag for the file to differentiate it from other HUMAnN outputs\n")
-    if args.graphlan_export == 'true':
+    if args.graphlan_export == 'yes':
         sconstruct_file.write("c_aastrExport = [\n")
-        sconstruct_file.write(" [r'04b.*mpt', [[c_strProgGraphlanTree, [c_strFileGraphlan]]], \"-graphlan_tree\"],\n")
-        sconstruct_file.write(" [r'04b.*mpm', [c_strProgGraphlanTree, [c_strFileGraphlan]]], \"-graphlan_tree\"],\n")
-        sconstruct_file.write(" [r'04b.*mpt', [[c_strProgGraphlanRings, [c_strFileGraphlan]]], \"-graphlan_rings\"],\n")
-        sconstruct_file.write(" [r'04b.*mpm', [[c_strProgGraphlanRings, [c_strFileGraphlan]]], \"-graphlan_rings\"],\n")
+        sconstruct_file.write(" [r'04b.*', [[c_strProgGraphlanTree, [c_strFileGraphlan]]], \"-graphlan_tree\"],\n")
+        sconstruct_file.write(" [r'04b.*', [[c_strProgGraphlanRings, [c_strFileGraphlan]]], \"-graphlan_rings\"],\n")
         sconstruct_file.write("]\n")
 
     sconstruct_file.write("\n")
@@ -230,14 +246,13 @@ def generate_outputs(tmp_output_dir,args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', required=True)
-    parser.add_argument('--output', required=True)
     parser.add_argument('--humann_dir', required=True)
     parser.add_argument('--cog_extracted_data')
     parser.add_argument('--ko_abundance', required=True)
     parser.add_argument('--kegg_pathway', required=True)
     parser.add_argument('--kegg_module', required=True)
     parser.add_argument('--metarep', required=True)
-    parser.add_argument('--metacyc_enz_abundance', required=True)
+    parser.add_argument('--metacyc_enzyme', required=True)
     parser.add_argument('--metacyc_pathway', required=True)
     parser.add_argument('--taxonomic_limitation', required=True)
     parser.add_argument('--wb_smoothing', required=True)
@@ -246,10 +261,24 @@ if __name__ == '__main__':
     parser.add_argument('--low_coverage_elimination', required=True)
     parser.add_argument('--abundance_computation', required=True)
     parser.add_argument('--graphlan_export', required=True)
-    args = parser.parse_args()
-    print args
+    parser.add_argument('--kegg_ko_abundance_file')
+    parser.add_argument('--cog_abundance_file') 
+    parser.add_argument('--kegg_pathway_coverage_file')
+    parser.add_argument('--kegg_pathway_abundance_file')
+    parser.add_argument('--kegg_module_coverage_file')
+    parser.add_argument('--kegg_module_abundance_file')
+    parser.add_argument('--metacyc_pathway_coverage_file')
+    parser.add_argument('--metacyc_pathway_abundance_file')
+    parser.add_argument('--kegg_pathway_abundance_graphlan_tree')
+    parser.add_argument('--kegg_pathway_abundance_graphlan_rings')
+    parser.add_argument('--kegg_module_abundance_graphlan_tree')
+    parser.add_argument('--kegg_module_abundance_graphlan_rings')
+    parser.add_argument('--metacyc_pathway_abundance_graphlan_tree')
+    parser.add_argument('--metacyc_pathway_abundance_graphlan_rings')
 
-    if args.cog_extracted_data == 'true':
+    args = parser.parse_args()
+
+    if args.cog_extracted_data == 'yes':
         formate_blast_output(args.input,
             args.humann_dir + '/cog_data/refseq_orga_id_correspondance')
         command = 'cp ' + args.humann_dir + '/cog_data/* ' 
