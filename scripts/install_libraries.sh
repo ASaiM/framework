b69ca205d46f3f0017e6046212dac2995bf39619
bebatut <berenice.batut@udamail.fr>
2015-09-18 10:20:40 +0200

    Some changes in proftpd installation

--- scripts/install_libraries.sh
+++ scripts/install_libraries.sh
@@ -4,12 +4,34 @@ current_dir=$PWD
 echo -e "(re)install proftpd with specific modules? (y/n) \c"
 read 
 if [ $REPLY == "y" ]; then
-    cd lib/proftpd
-    make clean
-    ./configure --disable-auth-file --disable-ncurses --disable-ident --disable-shadow --enable-openssl --with-modules=mod_sql:mod_sql_postgres:mod_sql_passwd
-    make
-    make install
-    cd $current_dir
+    echo -e "install from sources? (y/n) \c"
+    read 
+    if [ $REPLY == "y" ]; then
+        cd lib/proftpd
+        make clean
+        ./configure \
+            --prefix=/usr/local/ \
+            --disable-auth-file \
+            --disable-ncurses \
+            --disable-ident \
+            --disable-shadow \
+            --enable-openssl \
+            --with-modules=mod_sql:mod_sql_passwd:mod_sql_postgres 
+            #--with-includes=/usr/lib/postgresql/9.4/bin/ \
+            #--with-libraries=/usr/lib/postgresql/9.4/lib/
+        make
+        make install
+        cd $current_dir
+    else
+        sudo apt-get install proftpd-basic \
+            proftpd-mod-pgsql \
+            proftpd-mod-mysql 
+        echo 'LoadModule mod_sql.c' >> /etc/proftpd/modules.conf 
+        echo 'LoadModule mod_sql_passwd.c' >> /etc/proftpd/modules.conf 
+        echo 'LoadModule mod_sql_postgres.c' >> /etc/proftpd/modules.conf
+        cp config/proftpd.conf /etc/proftpd/proftpd.conf
+        echo "SQLNamedQuery                   LookupGalaxyUser SELECT \"email, (CASE WHEN substring(password from 1 for 6) = 'PBKDF2' THEN substring(password from 38 for 69) ELSE password END) AS password2,512,512,'"$PWD"/"$1"database/ftp/%U','/bin/bash' FROM galaxy_user WHERE email='%U'\"" >> /usr/local/etc/proftpd.conf 
+    fi
 fi
 cp config/proftpd.conf /usr/local/etc/proftpd.conf
 echo "SQLNamedQuery                   LookupGalaxyUser SELECT \"email, (CASE WHEN substring(password from 1 for 6) = 'PBKDF2' THEN substring(password from 38 for 69) ELSE password END) AS password2,512,512,'"$PWD"/"$1"database/ftp/%U','/bin/bash' FROM galaxy_user WHERE email='%U'\"" >> /usr/local/etc/proftpd.conf 
@@ -25,4 +47,5 @@ if ! which git-hg > /dev/null; then
     fi
 fi
 
+pip install -r requirements.txt
 
