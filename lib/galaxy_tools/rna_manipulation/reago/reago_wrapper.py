cee2fdae5ef8da9383a7bbebb0db7dff2d124d34
bebatut <berenice.batut@gmail.com>
2015-09-21 14:30:58 +0200

    Add input file formatting, finish wrapper, ...

--- lib/galaxy_tools/rna_manipulation/reago/reago_wrapper.py
+++ lib/galaxy_tools/rna_manipulation/reago/reago_wrapper.py
@@ -8,22 +8,33 @@ import re
 
 reago_dir = '/tools/rna_manipulation/reago/reago/'
 
-def launch_filter_input(args, ref_string, dirpath):
+def add_read_pair_num(input_filepath, output_filepath, read_pair_num):
+    with open(input_filepath,'r') as input_file:
+        with open(output_filepath,'w') as output_file:
+            for line in input_file:
+                if line[0] == '>':
+                    split_line = line.split()
+                    split_line[0] = split_line[0] + '.' + str(read_pair_num)
+                    output_file.write(' '.join(split_line) + '\n')
+                else:
+                    output_file.write(line)
+
+def launch_filter_input(args, dirpath):
     command = 'python ' + reago_dir + 'filter_input.py '
-    command += args.r1_input_sequence_file + ' ' + args.r2_input_sequence_file
-    command += dirpath
+    command += dirpath + 'reads.1 ' + dirpath + 'reads.2 '
+    command += dirpath + ' '
     command += reago_dir + '/cm ab 4'
     os.system(command)
 
-def launch_sortmerna(args, ref_string, dirpath):
+def launch_reago(args, dirpath):
     command = 'python ' + reago_dir + 'reago.py '
-    command += dirpath + '.fasta'
+    command += dirpath + 'filtered.fasta '
     command += dirpath
-    command += ' -l' + args.read_length
-    command += ' -o' + args.overlap
-    command += ' -e' + args.error_correction
-    command += ' -t' + args.tip_size 
-    command += ' -b' + args.path_finding_parameter
+    command += ' -l ' + args.read_length
+    command += ' -o ' + args.overlap
+    command += ' -e ' + args.error_correction
+    command += ' -t ' + args.tip_size 
+    command += ' -b ' + args.path_finding_parameter
     os.system(command)
 
 def test_file_presence(regular_expression, filepaths):
@@ -56,9 +67,9 @@ def generate_outputs(dirpath, args):
     output_files = os.listdir(dirpath)
 
     copy_file(test_file_presence('full_genes.fasta', 
-        output_files), dirpath, args.aligned_sequence_file)
+        output_files), dirpath, args.full_gene_sequence_file)
     copy_file(test_file_presence('fragments.fasta', 
-        output_files), dirpath, args.rejected_sequence_file)
+        output_files), dirpath, args.fragment_sequence_file)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -74,18 +85,23 @@ if __name__ == '__main__':
     parser.add_argument('--path_finding_parameter', required=True)
     args = parser.parse_args()
 
-    print args
-
-    dirpath, filename = os.path.split(os.path.abspath(args.output_sequence_file))
-    filename, file_format = filename.split('.')
-    dirpath += '/' + filename +'_reago_files/'
+    dirpath, r1_filename = os.path.split(os.path.abspath(args.r1_input_sequence_file))
+    dirpath, r2_filename = os.path.split(os.path.abspath(args.r2_input_sequence_file))
+    r1_filename, file_format = r1_filename.split('.')
+    r2_filename, file_format = r2_filename.split('.')
+    dirpath += '/' + r1_filename + '_' + r2_filename +'_reago_files/'
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
+
+    add_read_pair_num(args.r1_input_sequence_file, dirpath + 'reads.1', 1)
+    add_read_pair_num(args.r2_input_sequence_file, dirpath + 'reads.2', 2)
+
+    reago_dir = args.src_dir + reago_dir
 
     launch_filter_input(args, dirpath)
     launch_reago(args, dirpath)
     generate_outputs(dirpath, args)
-
+    #print args
 
 
 
