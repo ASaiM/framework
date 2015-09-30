85fe853e92939c0e19f14a3eeeed151d45e192c3
bebatut <berenice.batut@gmail.com>
2015-09-30 14:06:11 +0200

    Change script path

--- src/launch_galaxy.sh
+++ src/launch_galaxy.sh
@@ -5,14 +5,14 @@ mkdir tmp
 galaxy_dir="lib/galaxy/"
 
 # Prepare environment
-./scripts/install_libraries.sh $galaxy_dir #> tmp/install_libraries
+./src/install_libraries.sh $galaxy_dir #> tmp/install_libraries
 #if grep "Error" tmp/install_libraries > /dev/null ; then
 #    echo "Error with install_libraries.sh"
 #    exit
 #fi
 
 # Prepare galaxy
-./scripts/prepare_galaxy.sh $galaxy_dir #> tmp/prepare_galaxy
+./src/prepare_galaxy.sh $galaxy_dir #> tmp/prepare_galaxy
 #if grep "Error" tmp/prepare_galaxy > /dev/null ; then
 #    echo "Error with prepare_galaxy.sh"
 #    exit
