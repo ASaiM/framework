60e60b382cde4ec44bd33a80b0b2031d887bc5e7
bebatut <berenice.batut@gmail.com>
2015-10-08 15:00:35 +0200

    Change prepare_galaxy script by splitting

--- src/prepare_galaxy.sh
+++ src/prepare_galaxy.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 
-galaxy_dir=$1
+galaxy_dir="lib/galaxy/"
+galaxy_tool_dir=$galaxy_dir/tools/
+
+tool_dir=lib/galaxy_tools/
+db_dir=data/db/
+
 current_dir=$PWD
 
-# Prepare galaxy tool sections
-function create_tool_section_dir {
-    if [ ! -d $1 ]; then
-        mkdir -p $1
-    fi
-}
-
-create_tool_section_dir $galaxy_dir/tools/extract
-create_tool_section_dir $galaxy_dir/tools/concatenate
-create_tool_section_dir $galaxy_dir/tools/assemble_paired_end
-create_tool_section_dir $galaxy_dir/tools/manipulate_rna
-create_tool_section_dir $galaxy_dir/tools/usearch
-create_tool_section_dir $galaxy_dir/tools/RDPTools
-create_tool_section_dir $galaxy_dir/tools/cluster_sequences
-create_tool_section_dir $galaxy_dir/tools/search_similarity
-create_tool_section_dir $galaxy_dir/tools/assigne_taxonomy_to_non_rRNA
-create_tool_section_dir $galaxy_dir/tools/assigne_taxonomy_to_rRNA
-create_tool_section_dir $galaxy_dir/tools/analyze_metabolism
-
-# Prepary galaxy tools which are in git submodules
-./src/prepare_git_submodules.sh
-
-#cp -r lib/galaxy_tools/* $galaxy_dir/tools/
+# Prepare databases
+./src/prepare_databases.sh $db_dir
 
 # Prepare galaxy tools
-./src/prepare_galaxy_tools.sh $galaxy_dir
+./src/prepare_galaxy_tools.sh $galaxy_tool_dir $tool_dir $db_dir
 
 # Prepare galaxy config
 cp config/* $galaxy_dir/config/
