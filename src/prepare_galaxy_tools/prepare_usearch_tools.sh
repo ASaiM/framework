a3d22b3a9a711b5742181b0fa00c80703bf40929
bebatut <berenice.batut@gmail.com>
2015-10-14 13:46:39 +0200

    Add uchime_denovo and derep_prefix installation command

--- src/prepare_galaxy_tools/prepare_usearch_tools.sh
+++ src/prepare_galaxy_tools/prepare_usearch_tools.sh
@@ -10,3 +10,14 @@ section_dir=usearch
 
 echo "Usearch..."
 create_tool_section_dir $galaxy_tool_dir/$section_dir
+cp $tool_dir/$section_dir/usearch8.1.1756* $galaxy_tool_dir/$section_dir
+
+## derep_prefix
+echo " derep_prefix..."
+derep_prefix_dir=$section_dir/derep_prefix
+create_copy_tool_dir $tool_dir/$derep_prefix_dir $galaxy_tool_dir/$derep_prefix
+
+## derep_prefix
+echo " uchime_denovo..."
+derep_prefix_dir=$section_dir/uchime_denovo
+create_copy_tool_dir $tool_dir/$derep_prefix_dir $galaxy_tool_dir/$uchime_denovo
