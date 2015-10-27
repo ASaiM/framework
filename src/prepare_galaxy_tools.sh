e3b0e6993b9198f0df510ec064a21554c9643e8a
bebatut <berenice.batut@gmail.com>
2015-10-27 18:17:27 +0100

    Remove prepare concatenate tool script call and comment other

--- src/prepare_galaxy_tools.sh
+++ src/prepare_galaxy_tools.sh
@@ -10,50 +10,42 @@ echo "Prepare tools"
     $galaxy_tool_dir \
     $tool_dir
 
-./src/prepare_galaxy_tools/prepare_concatenate_tools.sh \
-    $galaxy_tool_dir \
-    $tool_dir
+#./src/prepare_galaxy_tools/prepare_assemble_paired_end_tools.sh \
+#    $galaxy_tool_dir \
+#    $tool_dir
 
-./src/prepare_galaxy_tools/prepare_assemble_paired_end_tools.sh \
-    $galaxy_tool_dir \
-    $tool_dir
+#./src/prepare_galaxy_tools/prepare_control_quality_tools.sh \
+#    $galaxy_tool_dir \
+#    $tool_dir
 
-./src/prepare_galaxy_tools/prepare_control_quality_tools.sh \
-    $galaxy_tool_dir \
-    $tool_dir
+#./src/prepare_galaxy_tools/prepare_cluster_sequences_tools.sh \
+#    $galaxy_tool_dir \
+#    $tool_dir
 
-./src/prepare_galaxy_tools/prepare_cluster_sequences_tools.sh \
-    $galaxy_tool_dir \
-    $tool_dir
+#./src/prepare_galaxy_tools/prepare_RDPTools_tools.sh \
+#    $galaxy_tool_dir \
+#    $tool_dir
 
-./src/prepare_galaxy_tools/prepare_usearch_tools.sh \
-    $galaxy_tool_dir \
-    $tool_dir
+#./src/prepare_galaxy_tools/prepare_genometools_tools.sh \
+#    $galaxy_tool_dir \
+#    $tool_dir
 
-./src/prepare_galaxy_tools/prepare_RDPTools_tools.sh \
-    $galaxy_tool_dir \
-    $tool_dir
+#./src/prepare_galaxy_tools/prepare_manipulate_rna_tools.sh \
+#    $galaxy_tool_dir \
+#    $tool_dir
 
-./src/prepare_galaxy_tools/prepare_genometools_tools.sh \
-    $galaxy_tool_dir \
-    $tool_dir
+#./src/prepare_galaxy_tools/prepare_assigne_taxonomy_to_non_rRNA_tools.sh \
+#    $galaxy_tool_dir \
+#    $tool_dir
 
-./src/prepare_galaxy_tools/prepare_manipulate_rna_tools.sh \
-    $galaxy_tool_dir \
-    $tool_dir
+#./src/prepare_galaxy_tools/prepare_assigne_taxonomy_to_rRNA_tools.sh \
+#    $galaxy_tool_dir \
+#    $tool_dir
 
-./src/prepare_galaxy_tools/prepare_assigne_taxonomy_to_non_rRNA_tools.sh \
-    $galaxy_tool_dir \
-    $tool_dir
-
-./src/prepare_galaxy_tools/prepare_assigne_taxonomy_to_rRNA_tools.sh \
-    $galaxy_tool_dir \
-    $tool_dir
-
-./src/prepare_galaxy_tools/prepare_analyze_metabolism_tools.sh \
-    $galaxy_tool_dir \
-    $tool_dir \
-    $db_dir
+#./src/prepare_galaxy_tools/prepare_analyze_metabolism_tools.sh \
+#    $galaxy_tool_dir \
+#    $tool_dir \
+#    $db_dir
 
 echo ""
 
