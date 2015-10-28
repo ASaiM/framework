32e971f26fdd1f3347d503cda199396bc3f2faba
bebatut <berenice.batut@gmail.com>
2015-10-28 14:01:14 +0100

    Remove sortmerna preparation and comment reago preparation

--- src/prepare_galaxy_tools/prepare_manipulate_rna_tools.sh
+++ src/prepare_galaxy_tools/prepare_manipulate_rna_tools.sh
@@ -11,53 +11,41 @@ section_dir=manipulate_rna
 echo "Manipulate RNA..."
 create_tool_section_dir $galaxy_tool_dir/$section_dir
 
-## sortmerna
-echo " SortMeRNA..."
-sortmerna_dir=$section_dir/sortmerna
-cd $tool_dir/$sortmerna_dir/sortmerna
-./build.sh >> $current_dir/tmp/sortmerna_build
-if grep "Error" $current_dir/tmp/sortmerna_build > /dev/null ; then
-    echo "Error with build for sortmerna"
-    exit
-fi
-cd $current_dir
-create_copy_tool_dir $tool_dir/$sortmerna_dir $galaxy_tool_dir/$sortmerna_dir
-
 ## infernal
-echo " infernal..."
-infernal_dir=$section_dir/infernal
-create_copy_tool_dir $tool_dir/$infernal_dir $galaxy_tool_dir/$infernal_dir
-if ! which cmsearch > /dev/null; then
-    echo "Infernal..."
-    echo -e "install infernal on machine (needed to use reago)? (y/n) \c"
-    read 
-    if [ $REPLY == "y" ]; then
-        cd $galaxy_tool_dir/$infernal_dir
-        tar xzf infernal-1.1.1.tar.gz
-        cd infernal-1.1.1
+#echo " infernal..."
+#infernal_dir=$section_dir/infernal
+#create_copy_tool_dir $tool_dir/$infernal_dir $galaxy_tool_dir/$infernal_dir
+#if ! which cmsearch > /dev/null; then
+#    echo "Infernal..."
+#    echo -e "install infernal on machine (needed to use reago)? (y/n) \c"
+#    read 
+#    if [ $REPLY == "y" ]; then
+#        cd $galaxy_tool_dir/$infernal_dir
+#        tar xzf infernal-1.1.1.tar.gz
+#        cd infernal-1.1.1
 
-        ./configure >> $current_dir/tmp/infernal_configure
-        if grep "Error" $current_dir/tmp/infernal_configure > /dev/null ; then
-            echo "Error with configure for Infernal"
-            exit
-        fi
+#        ./configure >> $current_dir/tmp/infernal_configure
+#        if grep "Error" $current_dir/tmp/infernal_configure > /dev/null ; then
+#            echo "Error with configure for Infernal"
+#            exit
+#        fi
 
-        make >> $current_dir/tmp/infernal_make
-        if grep "Error" $current_dir/tmp/infernal_make > /dev/null ; then
-            echo "Error with make for Infernal"
-            exit
-        fi 
+#        make >> $current_dir/tmp/infernal_make
+#        if grep "Error" $current_dir/tmp/infernal_make > /dev/null ; then
+#            echo "Error with make for Infernal"
+#            exit
+#        fi 
 
-        sudo make install >> $current_dir/tmp/infernal_make_install
-        if grep "Error" $current_dir/tmp/infernal_make_install > /dev/null ; then
-            echo "Error with make install for Infernal"
-            exit
-        fi
-        cd $current_dir
-    fi
-fi
+#        sudo make install >> $current_dir/tmp/infernal_make_install
+#        if grep "Error" $current_dir/tmp/infernal_make_install > /dev/null ; then
+#            echo "Error with make install for Infernal"
+#            exit
+#        fi
+#        cd $current_dir
+#    fi
+#fi
 
 ## reago
-echo " Reago..."
-reago_dir=$section_dir/reago/
-create_copy_tool_dir $tool_dir/$reago_dir $galaxy_tool_dir/$reago_dir
+#echo " Reago..."
+#reago_dir=$section_dir/reago/
+#create_copy_tool_dir $tool_dir/$reago_dir $galaxy_tool_dir/$reago_dir
