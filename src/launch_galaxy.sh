c0618113c484c08bfef78bb8ca979fdd749713d9
bebatut <berenice.batut@gmail.com>
2015-11-04 11:26:59 +0100

    Clean prepare and launch galaxy scripts

--- src/launch_galaxy.sh
+++ src/launch_galaxy.sh
@@ -1,44 +1,6 @@
 #!/bin/bash
-galaxy_dir="lib/galaxy/"
 
-reset=false
-for i 
-do 
-    if [ $i = '-reset' ]; then
-        reset=true
-    fi
-done
-
-if [ -d $galaxy_dir ]; then
-    if $reset; then
-        echo "Reset Galaxy"
-        sudo rm -rf $galaxy_dir
-    fi
-fi
+./src/prepare_galaxy.sh launch
 
 
-mkdir tmp
-
-git submodule init
-if $reset; then
-    git submodule foreach git reset --hard HEAD
-fi
-git submodule update
-if $reset; then
-    git submodule foreach "git checkout master; git pull"
-    git submodule foreach git clean -f
-fi
-
-
-# Prepare environment
-./src/install_libraries.sh 
-
-# Prepare galaxy
-./src/prepare_galaxy.sh $galaxy_dir
-
-rm -rf tmp
-
-# Launch galaxy
-cd $galaxy_dir
-sh run.sh >> run_galaxy
 
--- src/prepare_galaxy.sh
+++ src/prepare_galaxy.sh
@@ -11,18 +11,38 @@ function create_symlink {
     fi 
 }
 
-galaxy_dir=$1
+to_do=$1
+current_dir=$PWD
+lib_dir=lib
+
+mkdir tmp
+
+# Installing Galaxy
+# =================
+# Getting the latest revision with wget from GitHub is faster than cloning it
+cd lib/
+wget https://codeload.github.com/galaxyproject/galaxy/tar.gz/master
+tar -zxvf master | tail
+rm master
+cd ../
+
+galaxy_dir=$lib_dir/galaxy-master
 galaxy_tool_dir=$galaxy_dir/tools/
 
-tool_dir=lib/galaxy_tools/
-db_dir=data/db/
-
-current_dir=$PWD
+# Prepare environment
+# ===================
+git submodule init
+git submodule update
+./src/install_libraries.sh 
 
 # Prepare databases
+# =================
+db_dir=data/db/
 ./src/prepare_databases.sh $db_dir
 
 # Prepare galaxy tools
+# ====================
+tool_dir=$lib_dir/galaxy_tools/
 ./src/prepare_galaxy_tools.sh $galaxy_tool_dir $tool_dir $db_dir
 
 tool_dir=$PWD/$tool_dir
@@ -39,25 +59,37 @@ do
     fi 
 done
 
-# Prepare galaxy config
-
+# Configure Galaxy
+# ================
+# Configuration files
 for i in $( ls config/ )
 do
     create_symlink $galaxy_dir/config/$i $PWD/config/$i
 done
 
-# Prepare galaxy tool_data
+# Tool data
 for i in $( ls data/tool-data/ )
 do 
     create_symlink $galaxy_dir/tool-data/$i $PWD/data/tool-data/$i
 done
 
+# Dependencies
 if [ ! -d $galaxy_dir/dependency_dir ]; then
     mkdir $galaxy_dir/dependency_dir
 fi
 
-# Prepare ftp directory
+# FTP
 if [ ! -d $galaxy_dir/database/ftp ]; then
     mkdir $galaxy_dir/database/ftp
 fi
 
+rm -rf tmp
+
+# Launch Galaxy
+# =============
+cd $galaxy_dir
+if [ $to_do == 'launch' ] ; then
+    sh run.sh >> run_galaxy
+elif [ $to_do == 'run_test' ] ; then
+    sh run_tests.sh
+fi
