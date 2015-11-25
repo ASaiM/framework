3554bd20a4a97d5e5623da737efe6a070a2f4321
bebatut <berenice.batut@gmail.com>
2015-11-25 15:53:23 +0100

    Add postgresql db cleaning

--- src/clean_galaxy.sh
+++ src/clean_galaxy.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+echo "Remove virtualenv..."
 rm -rf venv
+
+echo "Drop database and user..."
+if psql ${DB_NAME} -c '' 2>&1; then
+    echo "database ${DB_NAME} exists"
+    dropdb ${DB_NAME}
+fi
+if psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='${DB_ROLE}'" | grep -q 1; then 
+    echo "role ${DB_ROLE} exists"
+    dropuser ${DB_ROLE}
+fi
