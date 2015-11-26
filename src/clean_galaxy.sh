2d88c7d2a53a5ff3061dfbb294a2f1670be191af
bebatut <berenice.batut@udamail.fr>
2015-11-26 17:20:46 +0100

    Add reference to script to clean postgres db in clean galaxy script

--- src/clean_galaxy.sh
+++ src/clean_galaxy.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 echo "Remove virtualenv..."
+echo "===================="
 rm -rf venv
+echo ""
 
 echo "Drop database and user..."
-if psql ${DB_NAME} -c '' 2>&1; then
-    echo "database ${DB_NAME} exists"
-    dropdb ${DB_NAME}
-fi
-if psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='${DB_ROLE}'" | grep -q 1; then 
-    echo "role ${DB_ROLE} exists"
-    dropuser ${DB_ROLE}
-fi
+echo "========================="
+./src/postgresql/clean_postgres_db.sh
