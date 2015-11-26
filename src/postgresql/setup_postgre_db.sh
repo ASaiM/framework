8c1ac71b5c1981c93cdf3472115674b64b581cec
bebatut <berenice.batut@udamail.fr>
2015-11-26 17:20:19 +0100

    Add reference to script to clean postgres db

--- src/postgresql/setup_postgre_db.sh
+++ src/postgresql/setup_postgre_db.sh
@@ -8,14 +8,7 @@ elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     command_prefix="sudo -u postgres"
 fi
 
-if $command_prefix psql ${DB_NAME} -c '' 2>&1; then
-    echo "database ${DB_NAME} exists"
-    $command_prefix dropdb ${DB_NAME}
-fi
-if $command_prefix psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='${DB_ROLE}'" | grep -q 1; then 
-    echo "role ${DB_ROLE} exists"
-    $command_prefix dropuser ${DB_ROLE}
-fi
+./src/postgresql/clean_postgres_db.sh
 
 $command_prefix createdb ${DB_NAME}
 $command_prefix psql ${DB_NAME} -c "CREATE USER galaxy WITH PASSWORD 'password'"
