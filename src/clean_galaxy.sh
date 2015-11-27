3e86535c81e1521b43013853c4877c823110bdaa
bebatut <berenice.batut@gmail.com>
2015-11-27 11:53:17 +0100

    Add cleaning of directories, add sudo to rm venv and add option to drop db

--- src/clean_galaxy.sh
+++ src/clean_galaxy.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
+. src/parse_yaml.sh
+eval $(parse_yaml src/misc/config.yml "")
+
+echo "Remove local galaxy dir..."
+echo "=========================="
+rm -rf $galaxy_dir
+echo ""
+
+echo "Remove local shed dir..."
+echo "=========================="
+rm -rf $shed_dir
+echo ""
 
 echo "Remove virtualenv..."
 echo "===================="
-rm -rf venv
+sudo rm -rf venv
 echo ""
 
 echo "Drop database and user..."
 echo "========================="
-./src/postgresql/clean_postgres_db.sh
+echo -e "drop database and users? (y/n)"
+read 
+if [ $REPLY == "y" ]; then
+    ./src/postgresql/clean_postgres_db.sh
+fi
