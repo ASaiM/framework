12c49d822b09d5ebbd8edae8b5e5814b3d75888f
bebatut <berenice.batut@gmail.com>
2016-01-25 10:36:07 +0100

    Extract galaxy installtion and add deamon for toolshed

--- src/launch_toolshed.sh
+++ src/launch_toolshed.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 . src/misc/parse_yaml.sh
+. src/install_galaxy.sh
+
+current_dir=$PWD
 
 eval $(parse_yaml src/misc/config.yml "")
 
-
 echo "Installing Galaxy"
 echo "================="
-# Getting the latest revision with wget from GitHub is faster than cloning it
-function install_galaxy {
-    wget https://codeload.github.com/galaxyproject/galaxy/tar.gz/master
-    tar -zxvf master --transform 's/'$local_galaxy_dir'/'$local_toolshed_dir'/' | tail
-    rm master
-}
 cd $lib_dir/
 if [[ ! -d $local_toolshed_dir ]]; then
-    install_galaxy
+    install_galaxy $local_toolshed_dir "toolshed"
 fi
 cd ../
 echo ""
 
 toolshed_dir=$lib_dir/$local_toolshed_dir
 
-echo "Configure Galaxy"
-echo "================"
+echo "Configure ToolShed"
+echo "=================="
 # Configuration files
 cp $PWD/$galaxy_conf_file_dir/tool_shed.ini $toolshed_dir/config/tool_shed.ini
 
 cd $toolshed_dir
-./run_tool_shed.sh
+./run_tool_shed.sh --daemon
+
+source .venv/bin/activate
+pip install -r $current_dir/requirements.txt
+
