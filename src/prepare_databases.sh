225b494db8b483986fd6a6add78318ee45ed6389
bebatut <berenice.batut@gmail.com>
2016-01-28 11:44:06 +0100

    Upgrate script to prepare databases
    - Add a script to retrieve installed revision and owner using bioblend

--- src/prepare_databases.sh
+++ src/prepare_databases.sh
@@ -6,16 +6,17 @@ source $galaxy_dir/.venv/bin/activate
 
 current_dir=$PWD
 
+gi_url="http://"$host":"$port
+
 echo "Prepare SortMeRNA databases..."
-revision=`python src/misc/parse_tool_playbook_yaml.py \
-    --file $tool_playbook_files_dir/pretreatments_tool_list.yaml \
-    --tool_name sortmerna \
-    --tool_function get_revision_number`
-owner=`python src/misc/parse_tool_playbook_yaml.py \
-    --file $tool_playbook_files_dir/pretreatments_tool_list.yaml \
-    --tool_name sortmerna \
-    --tool_function get_owner`
+declare RESULT=($(python src/get_installed_tool_info.py \
+    --tool_name "sortmerna" \
+    --gi_url $gi_url \
+    --api_key $master_api_key))
+revision=${RESULT[0]}
+owner=${RESULT[1]}
 sortmerna_db_dir=$galaxy_dir/dependency_dir/sortmerna/2.0/$owner/sortmerna/$revision/rRNA_databases/
+
 cd $sortmerna_db_dir
 indexdb_rna --ref rfam-5.8s-database-id98.fasta,rfam-5.8s-database-id98
 indexdb_rna --ref rfam-5s-database-id98.fasta,rfam-5s-database-id98
@@ -29,52 +30,37 @@ cd $current_dir
 echo ""
 
 echo "Prepare HUMAnN2 databases..."
-revision=`python src/misc/parse_tool_playbook_yaml.py \
-    --file $tool_playbook_files_dir/functional_assignation_tool_list.yaml \
-    --tool_name humann2 \
-    --tool_function get_revision_number`
-owner=`python src/misc/parse_tool_playbook_yaml.py \
-    --file $tool_playbook_files_dir/functional_assignation_tool_list.yaml \
-    --tool_name humann2 \
-    --tool_function get_owner`
+declare RESULT=($(python src/get_installed_tool_info.py \
+    --tool_name "humann2" \
+    --gi_url $gi_url \
+    --api_key $master_api_key))
+revision=${RESULT[0]}
+owner=${RESULT[1]}
 humann2_db_dir=$galaxy_dir/dependency_dir/humann2/2.0/$owner/humann2/$revision/
-cd $humann2_db_dir
 
+cd $humann2_db_dir
 humann2_databases --download chocophlan full databases/
 humann2_databases --download uniref diamond databases/
-
 cd $current_dir
 echo ""
 
 ## retrieve Greengenes for QIIME
-echo "Greengenes downloading, extracting and formating..."
-revision=`python src/misc/parse_tool_playbook_yaml.py \
-    --file $tool_playbook_files_dir/functional_assignation_tool_list.yaml \
-    --tool_name qiime \
-    --tool_function get_revision_number`
-owner=`python src/misc/parse_tool_playbook_yaml.py \
-    --file $tool_playbook_files_dir/functional_assignation_tool_list.yaml \
-    --tool_name qiime \
-    --tool_function get_owner`
-greengenes_dir=$galaxy_dir/dependency_dir/qiime/1.9.1/$owner/qiime/$revision/databases
-cd $greengenes_dir
-wget ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_8_otus.tar.gz
-tar xzf gg_13_8_otus.tar.gz
-rm gg_13_8_otus.tar.gz
+echo "Prepare QIIME databases..."
+declare RESULT=($(python src/get_installed_tool_info.py \
+    --tool_name "qiime" \
+    --gi_url $gi_url \
+    --api_key $master_api_key))
+revision=${RESULT[0]}
+owner=${RESULT[1]}
+qiime_db_dir=$galaxy_dir/dependency_dir/qiime/1.9.1/$owner/qiime/$revision/databases
+cd $qiime_db_dir
 
-## retrieve SILVA for QIIME
-echo "SILVA downloading, extracting and formating..."
-revision=`python src/misc/parse_tool_playbook_yaml.py \
-    --file $tool_playbook_files_dir/functional_assignation_tool_list.yaml \
-    --tool_name qiime \
-    --tool_function get_revision_number`
-owner=`python src/misc/parse_tool_playbook_yaml.py \
-    --file $tool_playbook_files_dir/functional_assignation_tool_list.yaml \
-    --tool_name qiime \
-    --tool_function get_owner`
-silva_dir=$galaxy_dir/dependency_dir/qiime/1.9.1/$owner/qiime/$revision/databases
-silva_dir=$db_dir/greengenes/
-cd $silva_dir
+echo "  Greengenes downloading, extracting and formating..."
+#wget ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_8_otus.tar.gz
+#tar xzf gg_13_8_otus.tar.gz
+#rm gg_13_8_otus.tar.gz
+
+echo "  SILVA downloading, extracting and formating..."
 #wget ftp://greengenes.microbio.me/greengenes_release/gg_13_5/gg_13_8_otus.tar.gz
 #tar xzf gg_13_8_otus.tar.gz
 #rm gg_13_8_otus.tar.gz
