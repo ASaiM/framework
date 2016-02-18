d22e11de048b743e12345429325fb6bab3c0e4ff
bebatut <berenice.batut@udamail.fr>
2016-02-18 18:05:59 +0100

    Correct some errors in postgresql configuration

--- src/configure/postgresql/clean_postgres_db.sh
+++ src/configure/postgresql/clean_postgres_db.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 . src/misc.sh
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    command_prefix=""
-elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-    command_prefix="sudo -u postgres"
-fi
+get_postgresql_prefix
 
-if [[ ! -z `psql -lqt | cut -d \| -f 1 | grep -w $db_name` ]]; then
+if [[ ! -z `$command_prefix psql -lqt | cut -d \| -f 1 | grep -w $db_name` ]]; then
     echo "Database $db_name exists. Do you want to drop it? (y/N)"
     read answer
     if (( $answer == "y" )) || (( $answer == "Y" )); then
-        echo "  Drop $db_name"
+        echo "  Drop $db_name db"
         $command_prefix dropdb $db_name
     fi
 fi
 
-if [[ ! -z `psql -lqt | cut -d \| -f 1 | grep -w $toolshed_db_name` ]]; then
+if [[ ! -z `$command_prefix psql -lqt | cut -d \| -f 1 | grep -w $toolshed_db_name` ]]; then
     echo "Database $toolshed_db_name exists. Do you want to drop it? (y/N)"
     read answer
     if (( $answer == "y" )) || (( $answer == "Y" )); then
-        echo "  Drop $toolshed_db_name"
+        echo "  Drop $toolshed_db_name db"
         $command_prefix dropdb $toolshed_db_name
     fi
 fi
@@ -29,7 +25,7 @@ if $command_prefix psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$db
     echo "Role $db_role exists. Do you want to drop it? (Y/n)"
     read answer
     if (( $answer == "y" )) || (( $answer == "Y" )); then
-        echo "  Drop $db_role"
+        echo "  Drop $db_role role"
         $command_prefix dropuser $db_role
     fi
 fi
