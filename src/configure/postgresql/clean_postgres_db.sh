33bb53110b8a024a3b8adf0b9aba469686ffaef9
bebatut <berenice.batut@gmail.com>
2016-02-19 14:41:10 +0100

    Correct answer test in script to clean postgres db

--- src/configure/postgresql/clean_postgres_db.sh
+++ src/configure/postgresql/clean_postgres_db.sh
@@ -6,7 +6,7 @@ get_postgresql_prefix
 if [[ ! -z `$command_prefix psql -lqt | cut -d \| -f 1 | grep -w $db_name` ]]; then
     echo "Database $db_name exists. Do you want to drop it? (y/N)"
     read answer
-    if (( $answer == "y" )) || (( $answer == "Y" )); then
+    if [[ $answer == "y" || $answer == "Y" ]]; then
         echo "  Drop $db_name db"
         $command_prefix dropdb $db_name
     fi
@@ -15,7 +15,7 @@ fi
 if [[ ! -z `$command_prefix psql -lqt | cut -d \| -f 1 | grep -w $toolshed_db_name` ]]; then
     echo "Database $toolshed_db_name exists. Do you want to drop it? (y/N)"
     read answer
-    if (( $answer == "y" )) || (( $answer == "Y" )); then
+    if [[ $answer == "y" || $answer == "Y" ]]; then
         echo "  Drop $toolshed_db_name db"
         $command_prefix dropdb $toolshed_db_name
     fi
@@ -24,7 +24,7 @@ fi
 if $command_prefix psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$db_role'" | grep -q 1; then 
     echo "Role $db_role exists. Do you want to drop it? (Y/n)"
     read answer
-    if (( $answer == "y" )) || (( $answer == "Y" )); then
+    if [[ $answer == "y" || $answer == "Y" ]]; then
         echo "  Drop $db_role role"
         $command_prefix dropuser $db_role
     fi
