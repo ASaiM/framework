fa2da3674ef30d8087b0be34933aec2fe02d9931
Bérénice Batut <bebatut@gmail.com>
2016-03-02 10:35:26 +0100

    Improve script to fet process stats

--- src/get_process_stat.sh
+++ src/get_process_stat.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-echo "Started time: " 
-echo $(date +%r)
+echo "Started time: " $(date +%r)
 
 pid=$1
-stat_file="stat_for_"$pid
+stat_file="stat_for_"$pid".txt"
 
 if [[ -f $stat_file ]]; then
     rm $stat_file
 fi
 
 touch $stat_file
+echo "Time %CPU Size" >> $stat_file
 while true; do
     echo -n $(date +%r) >> $stat_file
+    echo -n " " >> $stat_file
     echo -n `ps --format pcpu,size --no-headers $pid` >> $stat_file
     echo >> $stat_file
     sleep 1
