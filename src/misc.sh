5d416d337334a4c980081225f6f6cc59ed2ad399
bebatut <berenice.batut@gmail.com>
2016-04-12 15:43:59 +0200

    Change generate_tools_yml function for ansible tools playbook

--- src/misc.sh
+++ src/misc.sh
@@ -34,12 +34,13 @@ function generate_tools_yml {
     touch $1
     echo "- hosts: localhost" >> $1
     echo "  connection: local" >> $1
+    echo "  vars:"
+    echo "      galaxy_tools_galaxy_instance_url: http://$host:$port/" >> $1
+    echo "      galaxy_tools_api_key: $master_api_key" >> $1
+    echo "      galaxy_tools_tool_list_files: [ \"files/common_tool_list.yaml\", \"files/pretreatments_tool_list.yaml\", \"files/taxonomic_assignation_tool_list.yaml\", \"files/functional_assignation_tool_list.yaml\", \"files/posttreatments_tool_list.yaml\"]" >> $1 
+    echo "      galaxy_server_dir: $PWD/$galaxy_dir/" >> $1
     echo "  roles:" >> $1
     echo "      - role: ansible-galaxy-tools" >> $1
-    echo "        galaxy_tools_galaxy_instance_url: http://$host:$port/" >> $1
-    echo "        galaxy_tools_api_key: $master_api_key" >> $1
-    echo "        galaxy_tools_tool_list_files: [ \"files/common_tool_list.yaml\", \"files/pretreatments_tool_list.yaml\", \"files/taxonomic_assignation_tool_list.yaml\", \"files/functional_assignation_tool_list.yaml\", \"files/posttreatments_tool_list.yaml\"]" >> $1 
-    echo "        galaxy_server_dir: $PWD/$galaxy_dir/" >> $1
 }
 
 function generate_galaxy_ini {
