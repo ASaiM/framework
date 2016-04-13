f971ff37b131d6385421593a268109d7f6c058b7
Bérénice Batut <bebatut@gmail.com>
2016-04-13 10:46:15 +0200

    Correct function to generate tools.yml for ansible

--- src/misc.sh
+++ src/misc.sh
@@ -33,14 +33,15 @@ function generate_tools_yml {
     fi
     touch $1
     echo "- hosts: localhost" >> $1
+    echo "  gather_facts: False" >> $1
     echo "  connection: local" >> $1
-    echo "  vars:"
+    echo "  vars:" >> $1
     echo "      galaxy_tools_galaxy_instance_url: http://$host:$port/" >> $1
     echo "      galaxy_tools_api_key: $master_api_key" >> $1
     echo "      galaxy_tools_tool_list_files: [ \"files/common_tool_list.yaml\", \"files/pretreatments_tool_list.yaml\", \"files/taxonomic_assignation_tool_list.yaml\", \"files/functional_assignation_tool_list.yaml\", \"files/posttreatments_tool_list.yaml\"]" >> $1 
     echo "      galaxy_server_dir: $PWD/$galaxy_dir/" >> $1
     echo "  roles:" >> $1
-    echo "      - role: ansible-galaxy-tools" >> $1
+    echo "      - ansible-galaxy-tools" >> $1
 }
 
 function generate_galaxy_ini {
