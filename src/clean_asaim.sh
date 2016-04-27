5e56b645c94b3a89b3d7d3558a08e5ebc6baa7ad
bebatut <berenice.batut@gmail.com>
2016-04-27 11:09:18 +0200

    Remove all local lib dir in clean script

--- src/clean_asaim.sh
+++ src/clean_asaim.sh
@@ -1,20 +1,16 @@
 #!/usr/bin/env bash
 . src/misc.sh
 
-echo "Remove local galaxy dir..."
-echo "=========================="
-rm -rf $galaxy_dir
-echo ""
-
-echo "Remove local shed dir..."
-echo "========================"
-rm -rf $shed_dir
-echo ""
-
 echo "Stop FTP server..."
 echo "=================="
 sudo kill -TERM `sudo cat $lib_dir/proftpd/var/proftpd.pid`
+echo ""
 
 echo "Drop database and user..."
 echo "========================="
 $src_configure/clean_postgres_db.sh
+
+echo "Remove local lib dir with Galaxy, proftpd, ansible playbook..."
+echo "=============================================================="
+rm -rf $lib
+echo ""
