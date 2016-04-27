2f73846fff034c250dba7d6762f829eba63216f8
Bérénice Batut <bebatut@gmail.com>
2016-04-27 11:12:44 +0200

    Correct lib path

--- src/clean_asaim.sh
+++ src/clean_asaim.sh
@@ -9,8 +9,9 @@ echo ""
 echo "Drop database and user..."
 echo "========================="
 $src_configure/clean_postgres_db.sh
+echo ""
 
 echo "Remove local lib dir with Galaxy, proftpd, ansible playbook..."
 echo "=============================================================="
-rm -rf $lib
+rm -rf $lib_dir
 echo ""
