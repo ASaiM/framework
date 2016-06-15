2ee442d9bde4647f040ee02de590e971182e96de
bebatut <berenice.batut@gmail.com>
2016-06-15 13:24:57 +0200

    Remove any command to deploy a local TS

--- src/configure/clean_postgres_db.sh
+++ src/configure/clean_postgres_db.sh
@@ -12,16 +12,7 @@ if [[ ! -z `$command_prefix psql -lqt | cut -d \| -f 1 | grep -w $db_name` ]]; t
     fi
 fi
 
-if [[ ! -z `$command_prefix psql -lqt | cut -d \| -f 1 | grep -w $toolshed_db_name` ]]; then
-    echo "Database $toolshed_db_name exists. Do you want to drop it? (y/N)"
-    read answer
-    if [[ $answer == "y" || $answer == "Y" ]]; then
-        echo "  Drop $toolshed_db_name db"
-        $command_prefix dropdb $toolshed_db_name
-    fi
-fi
-
-if $command_prefix psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$db_role'" | grep -q 1; then 
+if $command_prefix psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$db_role'" | grep -q 1; then
     echo "Role $db_role exists. Do you want to drop it? (Y/n)"
     read answer
     if [[ $answer == "y" || $answer == "Y" ]]; then
--- src/configure/setup_postgre_db.sh
+++ src/configure/setup_postgre_db.sh
@@ -7,7 +7,3 @@ $command_prefix createdb $db_name
 $command_prefix psql $db_name -c "CREATE USER $db_role WITH PASSWORD '$db_password'"
 $command_prefix psql $db_name -c "GRANT ALL PRIVILEGES ON DATABASE $db_name to $db_role"
 $command_prefix psql $db_name -c "SET CLIENT_ENCODING to 'UNICODE'"
-
-$command_prefix createdb $toolshed_db_name
-$command_prefix psql $toolshed_db_name -c "GRANT ALL PRIVILEGES ON DATABASE $toolshed_db_name to $db_role"
-$command_prefix psql $toolshed_db_name -c "SET CLIENT_ENCODING to 'UNICODE'"
