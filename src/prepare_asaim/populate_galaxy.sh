a3b84d0bcce5f4078fa4c7e0873ef38c84c402d3
bebatut <berenice.batut@gmail.com>
2016-06-21 15:11:53 +0200

    Add symlink for samtools

--- src/prepare_asaim/populate_galaxy.sh
+++ src/prepare_asaim/populate_galaxy.sh
@@ -24,3 +24,12 @@ do
     #    --gi_url "http://"$host":"$port \
     #    --api_key $master_api_key
 done
+
+declare RESULT=($(python $src_prepare/get_installed_tool_info.py \
+    --tool_name "package_samtools_1_2" \
+    --tool_shed "toolshed.g2.bx.psu.edu" \
+    --gi_url $gi_url \
+    --api_key $master_api_key))
+revision=${RESULT[0]}
+owner=${RESULT[1]}
+ln -s $galaxy_dir/dependency_dir/samtools/1.2/$owner/package_samtools_1_2/$revision/bin/samtools $galaxy_dir/.venv/bin/samtools
