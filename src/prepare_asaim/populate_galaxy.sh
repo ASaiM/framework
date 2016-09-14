55d0a568c05162ca065b77f8d9b55aabe9a516fe
keuv-grvl <k.gravouil@gmail.com>
2016-09-14 14:14:02 +0200

    '' was not set

--- src/prepare_asaim/populate_galaxy.sh
+++ src/prepare_asaim/populate_galaxy.sh
@@ -13,10 +13,12 @@ ansible-playbook tools.yml -vvv -i "localhost,"
 cd $current_dir
 echo ""
 
+gi_url="http://"$host":"$port
+
 declare RESULT=($(python $src_prepare/get_installed_tool_info.py \
     --tool_name "package_samtools_1_2" \
     --tool_shed "toolshed.g2.bx.psu.edu" \
-    --gi_url $gi_url \
+    --gi_url "$gi_url" \
     --api_key $master_api_key))
 revision=${RESULT[0]}
 owner=${RESULT[1]}
