5848a9c64744eacf8d90a86335e948ed17ef8346
Bérénice Batut <bebatut@gmail.com>
2017-01-25 09:04:19 +0100

    Correct path to workflows

--- src/prepare_asaim/import_workflows.py
+++ src/prepare_asaim/import_workflows.py
@@ -11,8 +11,8 @@ gi = galaxy.GalaxyInstance(url=url, email=admin_email, password=admin_pass)
 
 wf = galaxy.workflows.WorkflowClient(gi)
 
-wf.import_workflow_from_local_path('/home/galaxy/asaim_main_workflow.ga')
-wf.import_workflow_from_local_path('/home/galaxy/asaim_taxonomic_result_comparative_analysis.ga')
-wf.import_workflow_from_local_path('/home/galaxy/asaim_functional_result_comparative_analysis.ga')
-wf.import_workflow_from_local_path('/home/galaxy/asaim_go_slim_terms_comparative_analysis.ga')
-wf.import_workflow_from_local_path('/home/galaxy/asaim_taxonomically_related_functional_result_comparative_analysis.ga')
+wf.import_workflow_from_local_path('asaim_main_workflow.ga')
+wf.import_workflow_from_local_path('asaim_taxonomic_result_comparative_analysis.ga')
+wf.import_workflow_from_local_path('asaim_functional_result_comparative_analysis.ga')
+wf.import_workflow_from_local_path('asaim_go_slim_terms_comparative_analysis.ga')
+wf.import_workflow_from_local_path('asaim_taxonomically_related_functional_result_comparative_analysis.ga')
