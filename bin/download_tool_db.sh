5b52a81b6c78ecd569e1263d5f945fc2743f240a
Bérénice Batut <bebatut@gmail.com>
2017-03-19 14:57:17 +0100

    Add echo and python script to launch

--- bin/download_tool_db.sh
+++ bin/download_tool_db.sh
@@ -3,6 +3,9 @@
 mkdir /databases
 
 # Prepare SortMeRNA db
+echo ""
+echo "Prepare SortMeRNA db"
+echo ""
 mkdir /databases/rRNA_databases
 sed -i.bak 's/#rfam/rfam/' $GALAXY_ROOT/tool-data/toolshed.g2.bx.psu.edu/repos/rnateam/sortmerna/59252ca85c74/rRNA_databases.loc
 sed -i.bak 's/#silva/silva/' $GALAXY_ROOT/tool-data/toolshed.g2.bx.psu.edu/repos/rnateam/sortmerna/59252ca85c74/rRNA_databases.loc
@@ -21,6 +24,12 @@ rm -rf sortmerna-2.1b/
 /tool_deps/_conda/envs/__sortmerna\@2.1b/bin/indexdb_rna --ref /databases/rRNA_databases/silva-bac-23s-id98.fasta,/databases/rRNA_databases/silva-bac-23s-id98
 /tool_deps/_conda/envs/__sortmerna\@2.1b/bin/indexdb_rna --ref /databases/rRNA_databases/silva-euk-18s-id95.fasta,/databases/rRNA_databases/silva-euk-18s-id95
 /tool_deps/_conda/envs/__sortmerna\@2.1b/bin/indexdb_rna --ref /databases/rRNA_databases/silva-euk-28s-id98.fasta,/databases/rRNA_databases/silva-euk-28s-id98
+echo ""
+
+echo ""
+echo "Prepare MetaPhlan2 and HUMAnN2 db"
+echo ""
+"src/prepare_asaim/download_databases.sh"
 
 # Prepare MetaPhlAn db
 #mkdir /databases/db_v20
